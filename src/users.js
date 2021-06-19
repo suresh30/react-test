ff8dd70152e4cd078b022c9dc7c0f193f5f7a9d1
suresh30 <30630097+suresh30@users.noreply.github.com>
2021-06-19 22:12:04 +0530

    Create users.js

---
+++ src/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    username: "Demo1",
+    password: "Password123",
+  },
+  {
+    username: "Demo2",
+    password: "Password123",
+  },
+  {
+    username: "Demo3",
+    password: "Password123",
+  },
+];
+
+export default users;
