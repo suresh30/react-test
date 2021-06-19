57fdacd9425e69e68b680104483a0e24a4d4bfd7
suresh30 <30630097+suresh30@users.noreply.github.com>
2021-06-19 22:14:50 +0530

    Create actionTypes.js

---
+++ src/store/actionTypes.js
@@ -0,0 +1,11 @@
+export const LOGGED_IN = "LOGGED_IN";
+export const WRONG_DETAILS = "WRONG_DETAILS";
+export const SET_AUTH_REDIRECT_PATH = "SET_AUTH_REDIRECT_PATH";
+
+export const SET_PARAGRAPH = "SET_PARAGRAPH";
+export const LOAD_TABLE = "LOAD_TABLE";
+export const DELETE_TASK = "DELETE_TASK";
+export const ADD_TASK = "ADD_TASK";
+
+export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
+export const LOGOUT = "LOGOUT";
